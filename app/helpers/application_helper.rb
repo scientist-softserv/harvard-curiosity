99f7259d4958aa3bd03d11b0b708fdee8351ce74
Summer <73361970+summer-cook@users.noreply.github.com>
2022-05-20 09:23:10 -0500

    Theming updates (#80)
    * (First) new rails app bootstrapped with Spotlight
    
    * Non-Docker-specific changes
    
    * Dockerization
    
    * Allow seeded admin users to create Exhibits
    
    * Update README
    
    * get-local-build
    
    * update-for-deploy
    
    * adds-gitlab-secrets-check-helm
    
    * remove-gitlab-check-helm
    
    * update-staging-url
    
    * update-helm-deploy-db-env-values
    
    * revert-update-helm-values
    
    * remove-previous-value
    
    * update-translations-init
    
    * make-tmp-dir
    
    * Theming: bring in harvard assets and style homepage
    
    * update-dir-permission-for-deploy
    
    * update-tmp-folder-permissions-for-startup
    
    * start-from-scratch-dockerfile
    
    * Delete .env files, minor changes
    
    * Speed up Rails significantly by using volumes instead of mounting directories
    
    * Fix Docker Permissions
    
    * Exhibit homepage & exhibit search page
    
    * Landing Page Styling
    
    * style browse, feature and about pages
    
    * add line to precompile svgs
    
    * README and LICENSE updates from GitHub
    
    * Get svg logos working
    
    * Change exhibit featured thumbnail initial crop selection
    
    * Add search widgets back & style
    
    * Item show page and footer links
    
    * Add nil-check for find_asset method
    
    * remove only clause on build to remove detached mr state
    
    * 62 no thumbnail and viewer theme
    
    * SVGs round 2
    
    * Add back and style the constraints, move them to the right spot
    
    Co-authored-by: Benjamin Kiah Stroud <kiah@notch8.com>
    Co-authored-by: April Rieger <april@notch8.com>
    Co-authored-by: Rob Kaufman <rob@notch8.com>

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,25 @@
 module ApplicationHelper
   include SpotlightHelper
+
+  def embedded_svg(filename, options = {})
+    assets = Rails.application.assets
+    asset = assets&.find_asset(filename)
+
+    if asset
+      file = asset.source.force_encoding("UTF-8")
+      doc = Nokogiri::HTML::DocumentFragment.parse file
+      svg = doc.at_css "svg"
+      svg["class"] = options[:class] if options[:class].present?
+    else
+      doc = "<!-- SVG #{filename} not found -->"
+    end
+
+    raw doc
+  end
+
+  # add this into the application controller since it was only available in pages controller previously
+  # Insert soft breaks into email addresses so they wrap nicely
+  def render_contact_email_address(address)
+    mail_to address, sanitize(address).gsub(/([@\.])/, '\1<wbr />').html_safe
+  end
 end
