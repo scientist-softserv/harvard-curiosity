cb7a2f83673cdf26b29c69443293e9babfc394b1
Summer <summercook@bellsouth.net>
2022-07-26 01:48:29 +0000

    Fix pagination bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,15 @@ class ApplicationController < ActionController::Base
 
   layout :determine_layout if respond_to? :layout
 
+  helper_method :search_parameters?
+
   def not_found
     send_file 'public/404.html', disposition: 'inline', type: 'text/html; charset=utf-8', status: 404
+  end
+
+  # Check if any search parameters have been set
+  # @return [Boolean]
+  def search_parameters?
+    params[:search_field].present? || search_state.has_constraints?
   end
 end
