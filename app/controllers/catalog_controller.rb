9c51d279c1d9a328006b3d852639e20d6b6727c1
Jeremy Friesen <7423469-takeonrules@users.noreply.gitlab.com>
2022-08-18 20:31:13 +0000

    Refactoring full-text search

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -87,23 +87,49 @@ class CatalogController < ApplicationController
 
   # rubocop:disable Layout/LineLength, Metrics/MethodLength, Metrics/AbcSize
   def index
+    # If we have an empty string for "fulltext", do we need to worry about querying for full text?  There's implementation details in the view that assume if we pass the fulltext parameter
     if params[:fulltext]
-      ft_solr = RSolr.connect url: 'https://fts.lib.harvard.edu/solr/fts-collection'
+      # TODO: Consider parameterizing this with an ENV
+      ft_solr = RSolr.connect url: ENV.fetch("FTS_COLLECTION_URL") { 'https://fts.lib.harvard.edu/solr/fts-collection' }
       ft_response = ft_solr.get 'select', params: fts_solr_params
 
-      ft_document_list_raw = ft_response['grouped']['objectId']['groups']
       @ft_document_list = {}
-      ft_document_list_raw ||= []
+
+      ft_document_list_raw = ft_response.dig('grouped', 'objectId', 'groups') || []
       ft_document_list_raw.each do |g|
-        context = g['doclist']['docs'].first['content'][/.{0,100}#{params[:q]}.{0,100}/im]
-        urn = g['doclist']['docs'].first['urn'].upcase
-        @ft_document_list[urn] = g['doclist'].merge(context: context,
-                                                    fts_link: fts_search_link(g['groupValue'])).stringify_keys
+        doc = g['doclist']['docs'].first
+        next unless doc
+        urn = doc['urn']&.upcase
+        # NOTE: We need the URN for what we're building, but what happens if the resulting object doesn't
+        # have an URN?
+        #
+        # QUESTION: What is the desired behavior when an document doesn't have an URN?  Should we
+        # log a missing URN?
+        #
+        # NOTE: By skipping a record, we could be mis-reporting our pagination results.  We may say
+        # that there are 5 records when one record didn't have an URN, so we instead render 4.
+        next unless urn
+
+        # NOTE: We're getting the 'content' for the document (a string) and selecting (via a regular
+        # expression) the 100 characters before and after the "q" param.
+        context = doc['content'][/.{0,100}#{params[:q]}.{0,100}/im]
+
+        @ft_document_list[urn] = g['doclist'].merge("context" => context,
+                                                    "fts_link" => fts_search_link(g['groupValue']))
       end
       # use [:numFound] for 1 of XX results in this book
       # use [:context] for snippet of result
 
-      facet_fields = blacklight_config.facet_fields.values.map(&:field)[0..-2] + ['exhibit_test-exhibit_public_bsi']
+      # NOTE: We're removing the the last found facet and replacing it with the exhibit specific
+      # facet.  In the development environment, that facet was "exhibit_visibility".
+      #
+      # QUESTION: are we specifically wanting to remove the last facet or the "exhibit_visibility"?
+      #
+      # CONJECTURE: We are doing the following replacement to preserve the facet_field counts
+      # between the Blacklight query below and the SOLR query above.
+      #
+      # See https://github.com/projectblacklight/spotlight/blob/ebed5d6cd5413427d431e2db0705745ce648fff6/spec/models/spotlight/solr_document_sidecar_spec.rb#L17 for how this facet field name is generated.
+      facet_fields = blacklight_config.facet_fields.values.map(&:field)[0..-2] + ["exhibit_#{current_exhibit.slug}_public_bsi"]
       raw_response = ::Blacklight.default_index.connection.get('select',
                                                                params: { q: params[:q],
                                                                          facet: true,
@@ -111,9 +137,13 @@ class CatalogController < ApplicationController
                                                                          rows: fts_solr_params[:rows],
                                                                          start: fts_solr_params[:start] })
       sidecars = Spotlight::SolrDocumentSidecar.where(urn: @ft_document_list.keys, exhibit_id: current_exhibit.id)
+
       docs = sidecars.map do |sidecar|
-        sidecar.to_solr.stringify_keys if sidecar.present?
+        # NOTE: In a previous version, we had an `if sidecar.present?`; this did not appear
+        # necessary as the above query should only return objects that are "present?"
+        sidecar.to_solr.stringify_keys
       end
+
       raw_response['response']['docs'] = docs
       @response = ::Blacklight::Solr::Response.new(raw_response, raw_response['responseHeader'], blacklight_config: blacklight_config)
       @document_list = ActiveSupport::Deprecation::DeprecatedObjectProxy.new(@response.docs,
@@ -155,7 +185,7 @@ class CatalogController < ApplicationController
   # rubocop:enable Metrics/AbcSize
 
   def fts_search_link(object_id)
-    fts_link = 'https://fts.lib.harvard.edu/fts/search?'
+    fts_link = ENV.fetch("FTS_SEARCH_URL") { 'https://fts.lib.harvard.edu/fts/search' } + '?'
     fts_link += "query=#{params[:q] || '*'}"
     # including the set causes fts to ignore the ID
     # fts_link += "&S=#{current_exhibit.set_name.presence || '*'}"
