74d9e33f602ff08996e98d3f6810c079685159e6
Benjamin Kiah Stroud <kiah@notch8.com>
2022-08-24 16:03:26 -0700

    override spotlight to change Exhibit Tags facet label

---
+++ app/models/spotlight/blacklight_configuration_decorator.rb
@@ -0,0 +1,22 @@
+# OVERRIDE FILE from Spotlight v3.3.0
+
+module Spotlight
+  module BlacklightConfigurationDecorator
+    def add_exhibit_tags_fields(config)
+      # rubocop:disable Style/GuardClause
+      unless config.show_fields.include? :exhibit_tags
+        config.add_show_field :exhibit_tags, field: config.document_model.solr_field_for_tagger(exhibit),
+                                             link_to_facet: true,
+                                             separator_options: { words_connector: nil, two_words_connector: nil, last_word_connector: nil }
+      end
+
+      unless config.facet_fields.include? :exhibit_tags
+        # OVERRIDE: add :label key to make use of dynamic I18n
+        config.add_facet_field :exhibit_tags, field: config.document_model.solr_field_for_tagger(exhibit), limit: true, label: I18n.t('spotlight.search.fields.facet.exhibit_tags')
+      end
+      # rubocop:enable Style/GuardClause
+    end
+  end
+end
+
+Spotlight::BlacklightConfiguration.prepend(Spotlight::BlacklightConfigurationDecorator)
